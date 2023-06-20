2bb7a10c96e2f5a71859b5ae2b6677a12530c82a
Lucas Mengue <lucaskmengue@gmail.com>
2023-06-20 16:10:16 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -23,12 +23,26 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		upateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void upateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+
+	}
+
 }
